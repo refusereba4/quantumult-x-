7d47db984030274ac8c9d3ca4cd56295be128bef
Fan <64138071+refusereba4@users.noreply.github.com>
2023-08-13 20:49:25 +0800

    Update ximalaya.js

--- ximalaya.js
+++ ximalaya.js
@@ -2,7 +2,12 @@ var body = $response.body;
 var responseBody = JSON.parse(body);
 
 // 遍历header中的item项，找到title为818的项并删除
-responseBody.header = responseBody.header.filter(item => item.item.title !== '818');
+//responseBody.header = responseBody.header.filter(item => item.item.title !== '818');
+
+// 删除header中的所有item项
+if (responseBody['header']) {
+    responseBody['header'] = [];
+}
 
 body = JSON.stringify(responseBody);
 
